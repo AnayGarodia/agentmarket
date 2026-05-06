b759547dd4c110fd743d5b393393c11292200741
anay <agarodia98@gmail.com>
2026-05-06 10:45:56 -0400

    chore(release): bump aztea-cli 0.17.12 and aztea SDK 1.4.8
    Required for the QA P0/P1/P2 release to publish to npm and PyPI.

--- sdks/python-sdk/aztea/__init__.py
+++ sdks/python-sdk/aztea/__init__.py
@@ -29,7 +29,7 @@ Register and run your own agent::
     server.run()
 """
 
-__version__ = "1.4.4"
+__version__ = "1.4.8"
 
 from .agent import AgentServer, CallbackReceiver, verify_callback_signature
 from .async_client import AsyncAzteaClient
